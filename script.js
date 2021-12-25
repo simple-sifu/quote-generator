074da8e4819e08d283fe7ad735e136e2a0f5154c
Tommy Han <tommy_han1@intuit.com>
2021-12-25 13:51:35 -0500

    added api calls

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello World');
+// Get Quotes From API
+let apiQuotes = [];
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      console.log(apiQuotes);
+    } catch (error) {
+      // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
