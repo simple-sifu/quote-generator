59bbbead89763c75062a4780c78d737a9be4de53
Tommy Han <tommy_han1@intuit.com>
2021-12-25 14:08:25 -0500

    testing randow quotes

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Get Quotes From API
 let apiQuotes = [];
 
+// New Quote
+function newQuote() {
+  // Pick a random quotes from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  console.log(quote);
+}
+
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
       const response = await fetch(apiUrl);
       apiQuotes = await response.json();
-      console.log(apiQuotes);
+      newQuote();
     } catch (error) {
       // Catch Error Here
     }
