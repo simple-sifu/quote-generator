8b02066869660192a1a5523ab87a340e657d4956
Tommy Han <tommy_han1@intuit.com>
2021-12-25 19:47:15 -0500

    map buttons to new quote

--- script.js
+++ script.js
@@ -31,5 +31,15 @@ async function getQuotes() {
     }
 }
 
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
 // On Load
 getQuotes();
